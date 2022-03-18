a878c70951e827e80eb4aa20a00c82173d0f239e
nana <comfortappiah.ca@gmail.com>
2022-03-18 18:03:34 +0000

    express server configuration

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// configuring HTTP server
-const http = require("http");
+const express = require("express");
 
-const handleAllRequest = (requestObject, responseObject) => {
-
-
+const handleAllRequests = (requestObject, responseObject) => {
 	console.log("Hi i just received a request");
 	// console.log(requestObject);
 	// responseObject.write("htis is a response from the server \n");
@@ -13,7 +10,6 @@ const handleAllRequest = (requestObject, responseObject) => {
 	const url = requestObject.url;
 	if (url === "/") {
 		responseObject.write("<h1>welcome to the home page</h1>");
-
 	} else if (url === "/login") {
 		responseObject.write("<h2>this is login page</h2>");
 	} else if (url === "/signup") {
@@ -26,7 +22,40 @@ const handleAllRequest = (requestObject, responseObject) => {
 	responseObject.end();
 };
 
-const server = http.createServer(handleAllRequest);
-server.listen(3000, "127.0.0.1", () => {
-	console.log("Hi server is ready to accept a request");
-});
+const server = express();
+server.use(handleAllRequests);
+
+server.listen(3000, "127.0.0.1", () =>
+	console.log("server is ready to accept request")
+);
+
+// configuring HTTP server
+// const http = require("http");
+
+// const handleAllRequest = (requestObject, responseObject) => {
+// 	console.log("Hi i just received a request");
+// 	// console.log(requestObject);
+// 	// responseObject.write("htis is a response from the server \n");
+// 	// responseObject.write("this is your second response");
+// 	// responseObject.write("<h1>just trying hi tag in here</h1>");
+// 	responseObject.setHeader("Content-Type", "text/html");
+// 	const url = requestObject.url;
+// 	if (url === "/") {
+// 		responseObject.write("<h1>welcome to the home page</h1>");
+
+// 	} else if (url === "/login") {
+// 		responseObject.write("<h2>this is login page</h2>");
+// 	} else if (url === "/signup") {
+// 		responseObject.write("<h1>this is sign up page</h1>");
+// 	} else if (url === "/profile") {
+// 		responseObject.write("<h2>hey welcome to the profile page</2>");
+// 	} else {
+// 		responseObject.write("<h1>page not found</h1>");
+// 	}
+// 	responseObject.end();
+// };
+
+// const server = http.createServer(handleAllRequest);
+// server.listen(3000, "127.0.0.1", () => {
+// 	console.log("Hi server is ready to accept a request");
+// });
