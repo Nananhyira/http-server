8e8a171e75bb7e2df7ac0782ede09183299aac79
nana <comfortappiah.ca@gmail.com>
2022-03-19 00:08:03 +0000

    express routing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const handleAllRequests = (requestObject, responseObject) => {
 	console.log("Hi i just received a request");
@@ -8,21 +9,36 @@ const handleAllRequests = (requestObject, responseObject) => {
 	// responseObject.write("<h1>just trying hi tag in here</h1>");
 	responseObject.setHeader("Content-Type", "text/html");
 	const url = requestObject.url;
-	if (url === "/") {
-		responseObject.write("<h1>welcome to the home page</h1>");
-	} else if (url === "/login") {
-		responseObject.write("<h2>this is login page</h2>");
-	} else if (url === "/signup") {
-		responseObject.write("<h1>this is sign up page</h1>");
-	} else if (url === "/profile") {
-		responseObject.write("<h2>hey welcome to the profile page</2>");
-	} else {
-		responseObject.write("<h1>page not found</h1>");
-	}
+	// {
+	// 	responseObject.write("");
+
+	// 	responseObject.write("<h1>page not found</h1>");
+	// }
 	responseObject.end();
 };
 
+const handleAllHomeRequest = (req, res) => {
+	res.send("<h1>welcome to the home page</h1>");
+};
+const handleLoginRequest = (req, res) => {
+	res.send("<h2>this is login page</h2>");
+};
+const handleSignupRequest = (req, res) => {
+	res.send("<h1>this is sign up page</h1>");
+};
+const handleProfileRequest = (req, res) => {
+	res.send("<h2>hey welcome to the profile page</2>");
+};
+const handleDetailsRequest = (req, res) => {
+	res.send("<h1>this is the details page</h1>");
+};
+
 const server = express();
+server.use("/details", handleDetailsRequest);
+server.use("/profile", handleProfileRequest);
+server.use("/signup", handleSignupRequest);
+server.use("/login", handleLoginRequest);
+server.use("/", handleAllHomeRequest);
 server.use(handleAllRequests);
 
 server.listen(3000, "127.0.0.1", () =>
